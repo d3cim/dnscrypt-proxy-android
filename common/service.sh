2127b48580288b36a377cfb991864d99555b9e38
bluemeda <affif.bluemeda@gmail.com>
2018-03-07 00:31:52 +0700

    try resolve download.dnscrypt.info first

--- common/service.sh
+++ common/service.sh
@@ -6,12 +6,14 @@ MODDIR=${0%/*}
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
 
-$MODDIR/system/xbin/dnscrypt-proxy -config $MODDIR/system/etc/dnscrypt-proxy/dnscrypt-proxy.toml &
 while true
 do
-	ping -c 1 google.com
+	ping -c 1 download.dnscrypt.info
 	if [[ $? == 0 ]];
 	then
+		
+		$MODDIR/system/xbin/dnscrypt-proxy -config $MODDIR/system/etc/dnscrypt-proxy/dnscrypt-proxy.toml &
+		sleep 5
 		iptables -t nat -A OUTPUT -p tcp --dport 53 -j DNAT --to-destination 127.0.0.1:5353
 		iptables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:5353
 		break;
