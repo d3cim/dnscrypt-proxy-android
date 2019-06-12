42768a2d01a83f4e01ec1e9d082e96c1d469f1e3
George Daniel <51092143+lindroidux@users.noreply.github.com>
2019-06-12 16:39:05 +0000

    updated-service.sh
    updated-service.sh

--- common/service.sh
+++ common/service.sh
@@ -6,13 +6,14 @@
 # if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
+# change config directory from /system/etc to /data/media/0/dnscrypt-proxy
+# /sdcard is softlink to /data/media/0 in lineageos 16
+	while ! [ `pgrep -x dnscrypt-proxy` ] ; do
+		$MODDIR/system/bin/dnscrypt-proxy -config  /data/media/0/dnscrypt-proxy/dnscrypt-proxy.toml; 
+	done
 
-		# change config directory from /system/etc to /data/media/0/dnscrypt-proxy
-		# /sdcard is softlink to /data/media/0 in lineageos 16
-		$MODDIR/system/bin/dnscrypt-proxy -config  /data/media/0/dnscrypt-proxy/dnscrypt-proxy.toml 
-		
 #IPTABLES
-iptables -t nat -A OUTPUT -p tcp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
-iptables -t nat -A OUTPUT -p udp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
-ip6tables -t nat -A OUTPUT -p tcp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination [::1]:5354
-ip6tables -t nat -A OUTPUT -p udp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination [::1]:5354
+		iptables -t nat -A OUTPUT -p tcp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+		iptables -t nat -A OUTPUT -p udp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+		ip6tables -t nat -A OUTPUT -p tcp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination [::1]:5354
+		ip6tables -t nat -A OUTPUT -p udp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination [::1]:5354
