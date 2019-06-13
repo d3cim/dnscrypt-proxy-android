fb27a7c18a9b357d5b6972d7ce04ab02ae32a2af
George Daniel <51092143+lindroidux@users.noreply.github.com>
2019-06-13 08:47:22 +0000

    updated option.sh
    updated option.sh

--- common/option.sh
+++ common/option.sh
@@ -15,22 +15,3 @@ CONFIG_FILE=/data/media/0/dnscrypt-proxy/dnscrypt-proxy.toml
     sed -i -e 's/\[::1\]:53/\[::1\]:5354/g' $CONFIG_FILE
 
   fi
-
-
-# let dnscrypt-proxy do its job only. Let Auto-redirection be done by some other application.
-# ui_print " "
-# ui_print " Vol+ = Auto redirect DNS request with iptables"
-# ui_print " "
-# ui_print " Vol- = Set DNS manually with 3rd-party app"
-# ui_print " "
-
-# if $FUNCTION; then
-#   ui_print " "
-#   ui_print " Auto mode"
-# else
-#   ui_print " "
-#   ui_print " Manual mode"
-#   ui_print " deleting iptables rules"
-#   sed -i -e '/#IPTABLES/,$d' $TMPDIR/service.sh
-#   sed -i -e "s/'127.0.0.1.*'/'127.0.0.1:53', '[::1]:53'/g" /data/media/0/dnscrypt-proxy/dnscrypt-proxy.toml
-fi
