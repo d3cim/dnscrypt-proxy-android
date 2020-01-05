2f7f9e4af5992d1e11ec6b6084c81391f39e322b
quindecim <quindecim@airmail.cc>
2020-01-04 20:29:49 -0500

    Text optimization

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -8,7 +8,6 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 
-# Let dnscrypt-proxy do its job only.
 # iptables -t nat -A OUTPUT -p tcp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination 127.0.0.1:53
 # iptables -t nat -A OUTPUT -p udp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination 127.0.0.1:53
 # ip6tables -t nat -A OUTPUT -p tcp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination [::1]:53
