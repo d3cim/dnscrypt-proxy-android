32f4211d21f556f369b834c87f710fc5e2fc0aca
quindecim <quindecim@airmail.cc>
2020-04-17 14:51:22 -0400

    DROP IPv6 queries
    Can cause leaks with some ISPs

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -10,5 +10,6 @@ MODDIR=${0%/*}
 
 iptables -t nat -A OUTPUT -p tcp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination 127.0.0.1:53
 iptables -t nat -A OUTPUT -p udp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination 127.0.0.1:53
-# ip6tables -t nat -A OUTPUT -p tcp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination [::1]:53
-# ip6tables -t nat -A OUTPUT -p udp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination [::1]:53
+ip6tables -A OUTPUT -p tcp --dport 53 -j DROP
+ip6tables -A OUTPUT -p udp --dport 53 -j DROP
+
