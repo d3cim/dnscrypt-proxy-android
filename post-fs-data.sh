40b8b227a7460117a869e09782499bc51a19ffb2
quindecim <quindecim@firemail.cc>
2020-11-06 05:39:34 -0500

    [UPDATE] - Stop to drop IPv6 queries

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -10,6 +10,6 @@ MODDIR=${0%/*}
 
 iptables -t nat -A OUTPUT -p tcp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
 iptables -t nat -A OUTPUT -p udp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
-ip6tables -A OUTPUT -p tcp -j DROP
-ip6tables -A OUTPUT -p udp -j DROP
+# ip6tables -t nat -A OUTPUT -p tcp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination [::1]:5354
+# ip6tables -t nat -A OUTPUT -p udp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination [::1]:5354
 
