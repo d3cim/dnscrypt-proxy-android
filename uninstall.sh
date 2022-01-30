d9b00b75b5424db2a87b8dadf19b927f443108da
quindecim <quindecim@noreply.codeberg.org>
2022-01-30 12:58:37 +0100

    [CONFIG] Loops the command until the boot is complete and add more path variants to removal

--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,15 @@
+(
+while [ "$(getprop sys.boot_completed)" != "1" ] && [ ! -d "/storage/emulated/0/Android" ]; do
+  sleep 1
+done
+
+rm -rf /data/media/0/dnscrypt-proxy
+rm -rf /mnt/runtime/default/emulated/0/dnscrypt-proxy
+rm -rf /mnt/runtime/full/emulated/0/dnscrypt-proxy
+rm -rf /mnt/runtime/read/emulated/0/dnscrypt-proxy
+rm -rf /mnt/runtime/write/emulated/0/dnscrypt-proxy
+rm -rf /sdcard/dnscrypt-proxy
 rm -rf /storage/emulated/0/dnscrypt-proxy
+rm -rf /storage/self/primary/dnscrypt-proxy
+
+)&
