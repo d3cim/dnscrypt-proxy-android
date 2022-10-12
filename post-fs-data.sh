ca85ca49b0517100a834295f5c65207780890f42
quindecim <49964366+quindecim@users.noreply.github.com>
2022-10-13 01:19:27 +0200

    Add back automatic redirections

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -8,6 +8,12 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 
+# Redirect DNS requests to localhost
+iptables -t nat -A OUTPUT -p tcp ! -d 45.11.45.11 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+iptables -t nat -A OUTPUT -p udp ! -d 45.11.45.11 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+# ip6tables -t nat -A OUTPUT -p tcp ! -d 45.11.45.11 --dport 53 -j DNAT --to-destination [::1]:5354
+# ip6tables -t nat -A OUTPUT -p udp ! -d 45.11.45.11 --dport 53 -j DNAT --to-destination [::1]:5354
+
 # Force disable IPv6 OS connections
 resetprop net.ipv6.conf.all.accept_redirects 0
 resetprop net.ipv6.conf.all.disable_ipv6 1
