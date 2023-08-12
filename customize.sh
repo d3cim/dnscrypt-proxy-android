3fd78929eec35220f7459312eb725af61a570c54
quindecim <49964366+quindecim@users.noreply.github.com>
2023-08-12 15:49:15 +0200

    Update to 2.1.5

--- customize.sh
+++ customize.sh
@@ -1,7 +1,7 @@
   ui_print " "
   ui_print "******************************"
   ui_print "*   dnscrypt-proxy-android   *"
-  ui_print "*            2.1.4           *"
+  ui_print "*            2.1.5           *"
   ui_print "******************************"
   ui_print "*         quindecim          *"
   ui_print "******************************"
